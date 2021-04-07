a7d2a41f6ff97a977466c95f0e8edbe9d6c207bf
anhtudo97 <anhtudo97@gmail.com>
2021-04-07 23:58:08 +0700

    feat: init env

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+// import GlobalStyle from './components/GlobalStyle';
+
+ReactDOM.render(
+  <>
+    <h1>2048</h1>
+    {/* <GlobalStyle />
+    <App /> */}
+  </>,
+  document.getElementById('game'),
+);
