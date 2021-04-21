c392c8986b771159d731e3bd9a3d48714dd1d535
anhtudo97 <anhtudo97@gmail.com>
2021-04-21 23:21:23 +0700

    feat: localStorage

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,35 @@
+import { useCallback, useState } from 'react'
+
+
+const useLocalStorage = <T>(
+    name: string,
+    initalValue: T
+): [T, (newValue: T) => void] => {
+    const getValue = (): T => {
+        try {
+            const item = window.localStorage.getItem(name);
+            return item != null ? JSON.parse(item) : initalValue;
+        } catch (error) {
+            console.error(`Cannot get localStorage by the given name ${name}:`, error.message);
+            return initalValue;
+        }
+    }
+
+    const [storedValue, setStoredValue] = useState<T>(getValue);
+
+    const setValue = useCallback(
+        (newValue: T) => {
+            try {
+                window.localStorage.setItem(name, JSON.stringify(newValue));
+                setStoredValue(newValue);
+            } catch (error) {
+                console.error(`Cannot set localStorage by the given name ${name}:`, error.message);
+
+            }
+        }, [name]
+    )
+
+    return [storedValue, setValue];
+};
+
+export default useLocalStorage;
