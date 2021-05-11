661c76aaa512e6c60b46dea9f4a03c1fb7f75ee0
anhtudo97 <anhtudo97@gmail.com>
2021-05-11 17:04:08 +0700

    feat: run code

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,86 +1,148 @@
 import React, { FC, useCallback, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
-
 import Box from '../components/Box';
-
-import { useTheme } from '../hooks/useTheme';
+import Control from '../components/Control/Control';
+import GameBoard from '../components/GameBoard';
+import ScoreBoard from '../components/ScoreBoard';
+import Switch from '../components/Switch';
+import Text from '../components/Text';
 import useGameBoard from '../hooks/useGameBoard';
 import useGameScore from '../hooks/useGameScore';
 import useGameState, { GameStatus } from '../hooks/useGameState';
-import useLocalStorage from '../hooks/useLocalStorage';
 import useScaleControl from '../hooks/useScaleControl';
-import { ThemeName } from '../themes/types'
 import { GRID_SIZE, MIN_SCALE, SPACING } from '../utils/constants';
+import useLocalStorage from '../hooks/useLocalStorage';
+import { ThemeName } from '../themes/types';
+import { useTheme } from '../hooks/useTheme';
 
 export type Configuration = {
-    theme: ThemeName;
-    bestScore: number;
-    rows: number;
-    cols: number;
-}
+  theme: ThemeName;
+  bestScore: number;
+  rows: number;
+  cols: number;
+};
 
-const APP_NAME = '@)$*'
+const APP_NAME = 'react-2048';
 
 const App: FC = () => {
-    const [{ status: gameStatus, pause }, setGameStatus] = useGameState({
-        status: 'running',
-        pause: false,
-    });
-    const [config, setConfig] = useLocalStorage<Configuration>(APP_NAME, {
-        theme: 'default',
-        bestScore: 0,
-        rows: MIN_SCALE,
-        cols: MIN_SCALE,
-    });
+  const [{ status: gameStatus, pause }, setGameStatus] = useGameState({
+    status: 'running',
+    pause: false,
+  });
 
-    const [{ name: themeName, value: themeValue }, setTheme] = useTheme(
-        config.theme,
-    );
+  const [config, setConfig] = useLocalStorage<Configuration>(APP_NAME, {
+    theme: 'default',
+    bestScore: 0,
+    rows: MIN_SCALE,
+    cols: MIN_SCALE,
+  });
 
-    const [rows, setRows] = useScaleControl(config.rows);
-    const [cols, setCols] = useScaleControl(config.cols);
+  const [{ name: themeName, value: themeValue }, setTheme] = useTheme(
+    config.theme,
+  );
 
-    const { total, best, addScore, setTotal } = useGameScore(config.bestScore);
+  const [rows, setRows] = useScaleControl(config.rows);
+  const [cols, setCols] = useScaleControl(config.cols);
 
-    const { tiles, onMove, onMovePending } = useGameBoard({
-        rows,
-        cols,
-        pause,
-        gameStatus,
-        setGameStatus,
-        addScore,
-    });
+  const { total, best, addScore, setTotal } = useGameScore(config.bestScore);
 
-    const onResetGame = useCallback(() => {
-        setGameStatus('restart');
-    }, [setGameStatus]);
+  const { tiles, onMove, onMovePending } = useGameBoard({
+    rows,
+    cols,
+    pause,
+    gameStatus,
+    setGameStatus,
+    addScore,
+  });
 
-    const onCloseNotification = useCallback(
-        (currentStatus: GameStatus) => {
-            setGameStatus(currentStatus === 'win' ? 'continue' : 'restart');
-        },
-        [setGameStatus],
-    );
+  const onResetGame = useCallback(() => {
+    setGameStatus('restart');
+  }, [setGameStatus]);
 
-    useEffect(() => {
-        if (gameStatus === 'restart') setTotal(0);
-    }, [gameStatus, setTotal]);
+  const onCloseNotification = useCallback(
+    (currentStatus: GameStatus) => {
+      setGameStatus(currentStatus === 'win' ? 'continue' : 'restart');
+    },
+    [setGameStatus],
+  );
 
-    useEffect(() => {
-        setConfig({ rows, cols, bestScore: best, theme: themeName });
-    }, [rows, cols, best, themeName, setConfig]);
+  useEffect(() => {
+    if (gameStatus === 'restart') setTotal(0);
+  }, [gameStatus, setTotal]);
 
-    return <ThemeProvider theme={themeValue}>
+  useEffect(() => {
+    setConfig({ rows, cols, bestScore: best, theme: themeName });
+  }, [rows, cols, best, themeName, setConfig]);
+
+  return (
+    <ThemeProvider theme={themeValue}>
+      <Box
+        justifyContent="center"
+        inlineSize="100%"
+        blockSize="100%"
+        alignItems="start"
+        borderRadius={0}
+      >
         <Box
-            justifyContent="center"
-            inlineSize="100%"
-            blockSize="100%"
-            alignItems="start"
-            borderRadius={0}
+          justifyContent="center"
+          flexDirection="column"
+          inlineSize={`${GRID_SIZE}px`}
         >
-
+          <Box marginBlockStart="s5" inlineSize="100%" justifyContent="end">
+            <Switch
+              title="dark mode"
+              value={themeName}
+              activeValue="dark"
+              inactiveValue="default"
+              knobColor="background"
+              activeColor="white"
+              inactiveColor="black"
+              onChange={setTheme}
+            />
+          </Box>
+          <Box inlineSize="100%" justifyContent="space-between">
+            <Box>
+              <Text fontSize={64} fontWeight="bold" color="primary">
+                2048
+              </Text>
+            </Box>
+            <Box justifyContent="center">
+              <ScoreBoard total={total} title="score" />
+              <ScoreBoard total={best} title="best" />
+            </Box>
+          </Box>
+          <Box marginBlockStart="s3" marginBlockEnd="s6" inlineSize="100%">
+            <Control
+              rows={rows}
+              cols={cols}
+              onReset={onResetGame}
+              onChangeRow={setRows}
+              onChangeCol={setCols}
+            />
+          </Box>
+          <GameBoard
+            tiles={tiles}
+            boardSize={GRID_SIZE}
+            rows={rows}
+            cols={cols}
+            spacing={SPACING}
+            gameStatus={gameStatus}
+            onMove={onMove}
+            onMovePending={onMovePending}
+            onCloseNotification={onCloseNotification}
+          />
+          <Box marginBlock="s4" justifyContent="center" flexDirection="column">
+            <Text fontSize={16} as="p" color="primary">
+              ✨ Join tiles with the same value to get 2048
+            </Text>
+            <Text fontSize={16} as="p" color="primary">
+              🕹️ Play with arrow keys or swipe
+            </Text>
+          </Box>
         </Box>
+      </Box>
     </ThemeProvider>
-}
+  );
+};
 
 export default App;
